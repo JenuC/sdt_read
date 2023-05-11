fecca45a1f5e542fa6be6e1037ec5b7691f43cec
JenuC <jenuvchacko@gmail.com>
2023-05-11 17:12:43 -0500

    read openscan sdt files

---
+++ read_openscan_sdt.py
@@ -0,0 +1,86 @@
+import sdtfile
+import io
+import zipfile
+import numpy as np
+
+def read_sdt_openscan(filename,print_on=False):
+    """returns [data],t,[mblocks,bheads,info]"""
+
+    with open(filename,'rb') as fid:
+
+        ## HEADER
+        header = np.rec.fromfile(fid, dtype=sdtfile.sdtfile.FILE_HEADER,
+                                     shape=1, byteorder='<')[0]
+        
+        if print_on:
+            #FH = sdtfile.sdtfile.FILE_HEADER
+            #FH= [i[0] for i in FH]
+            for j in (zip(FH,header)):
+                print(j)
+
+        ## INFO
+        fid.seek(header.info_offset)
+        info = fid.read(header.info_length).decode('windows-1250')
+        info = info.replace('\r\n', '\n')
+        if print_on:
+            print(info)
+
+        ## SETUP
+        fid.seek(header.setup_offs)
+        fsetup = sdtfile.SetupBlock(fid.read(header.setup_length)) # cant read this !
+
+        ## MEAS_BLOCK
+        fh=fid.seek(header.meas_desc_block_offset)    
+        mblocks=[]
+        for measblock_index in range(header.no_of_meas_desc_blocks):
+            #fmeas_blk = fid.read()
+            mblocks.append(np.rec.fromfile(fid, dtype=np.dtype(sdtfile.sdtfile.MEASURE_INFO), shape=1, byteorder='<'))
+            fid.seek(header.meas_desc_block_length - np.dtype(sdtfile.sdtfile.MEASURE_INFO).itemsize, 1)
+
+        if print_on:
+            #MH = sdtfile.sdtfile.MEASURE_INFO
+            #MH= [i[0] for i in MH]
+            for j in zip(mblocks[0].dtype.names,mblocks[0][0]):
+                print(j)
+  
+        # assuming sinilar channels
+        adc_re = int(mblocks[0].adc_re)
+        image_x = int(mblocks[0].image_x)
+        image_y = int(mblocks[0].image_y)
+        t = np.arange(adc_re, dtype='float64')
+        t *= mblocks[0].tac_r / float(mblocks[0].tac_g * adc_re)
+
+        ## BLOCK HEADER AND DATABLOCKS :
+        bheads=[]
+        data_list=[]
+        offset = header.data_block_offset
+        for datablock_index in range(header.no_of_data_blocks):
+
+            fid.seek(offset)
+            bh=(np.rec.fromfile(fid, dtype=sdtfile.sdtfile.BLOCK_HEADER_15, shape=1, byteorder='<')[0])
+            bheads.append(bh)
+            fid.seek(bh.data_offs)  
+
+            bt = sdtfile.sdtfile.BlockType(bh.block_type)
+            dtype = bt.dtype
+            dsize = bh.block_length // dtype.itemsize
+
+            if print_on:
+                print(datablock_index)
+                for j in zip(bh.dtype.names,bh):
+                    print(j)       
+
+            if sdtfile.sdtfile.BlockType(bh.block_type).compress:
+                    bio = io.BytesIO(fid.read(bh.next_block_offs - bh.data_offs))
+                    with zipfile.ZipFile(bio) as zf:
+                        data = zf.read('data_block')
+                    del bio
+                    data = np.frombuffer(data, dtype=dtype, count=dsize)
+            else:
+                    data = np.fromfile(fh, dtype=dtype, count=dsize)
+
+            data = data.reshape(image_x, image_y, adc_re)
+            data_list.append(data)
+
+            offset = bh.next_block_offs
+        return(data_list,t,[mblocks,bheads,info])

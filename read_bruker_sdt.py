069e8ba6b7976af246857ebda16adaf5e481d3c2
JenuC <jenuvchacko@gmail.com>
2025-04-02 10:42:56 -0500

    reflecting changes from sdt-file.py that use shorter rec-names from offset -> offs (v2023.9.28)

--- read_bruker_sdt.py
+++ read_bruker_sdt.py
@@ -4,81 +4,108 @@ import zipfile
 
 
 def read_sdt_info_brukerSDT(filename):
-    """ 
+    """
     modified from CGohlke sdtfile.py to read bruker 150 card data
     gives tarr, x.shape,y.shape,t.shape,c.shape
     """
     ## HEADER
-    with open(filename, 'rb') as fh:
-        header = np.rec.fromfile(fh, dtype=sdtfile.sdtfile.FILE_HEADER, shape=1, byteorder='<')
-        
+    with open(filename, "rb") as fh:
+        header = np.rec.fromfile(
+            fh, dtype=sdtfile.sdtfile.FILE_HEADER, shape=1, byteorder="<"
+        )
+
     measure_info = []
     dtype = np.dtype(sdtfile.sdtfile.MEASURE_INFO)
-    with open(filename, 'rb') as fh:
-        fh.seek(header.meas_desc_block_offset[0])
+    with open(filename, "rb") as fh:
+        if "meas_desc_block_offset" in header.dtype.names:
+            fh.seek(header.meas_desc_block_offset[0])
+        elif "meas_desc_block_offs" in header.dtype.names:
+            fh.seek(header.meas_desc_block_offs[0])
+        else:
+            raise ValueError(
+                "Header does not contain meas_desc_block_offset or meas_desc_block_offs"
+            )
         for _ in range(header.no_of_meas_desc_blocks[0]):
             measure_info.append(
-                np.rec.fromfile(fh, dtype=dtype, shape=1, byteorder='<'))
+                np.rec.fromfile(fh, dtype=dtype, shape=1, byteorder="<")
+            )
             fh.seek(header.meas_desc_block_length[0] - dtype.itemsize, 1)
-    
+
     times = []
     block_headers = []
 
-    try:
-        routing_channels_x = measure_info[0]['image_rx'][0]
-    except:
-        routing_channels_x = 1
-
-    offset = header.data_block_offset[0]
-
-    #print()
-    with open(filename, 'rb') as fh:
-        for _ in range(header.no_of_data_blocks[0]): ## 
+    if "data_block_offset" in header.dtype.names:
+        offset = header.data_block_offset[0]
+    elif "data_block_offs" in header.dtype.names:
+        offset = header.data_block_offs[0]
+    else:
+        raise ValueError("Header does not contain data_block_offset or data_block_offs")
+    # print()
+    with open(filename, "rb") as fh:
+        for _ in range(header.no_of_data_blocks[0]):  ##
             fh.seek(offset)
             # read data block header
-            bh = np.rec.fromfile(fh, dtype=sdtfile.sdtfile.BLOCK_HEADER, shape=1,
-                                 byteorder='<')[0]
+            bh = np.rec.fromfile(
+                fh, dtype=sdtfile.sdtfile.BLOCK_HEADER, shape=1, byteorder="<"
+            )[0]
             block_headers.append(bh)
             # read data block
             mi = measure_info[bh.meas_desc_block_no]
-            
+
             dtype = sdtfile.sdtfile.BlockType(bh.block_type).dtype
             dsize = bh.block_length // dtype.itemsize
-            
-            t = np.arange(mi.adc_re[0], dtype=np.float64)
-            t *= mi.tac_r / float(mi.tac_g * mi.adc_re)
+
+            # t = np.arange(mi.adc_re[0], dtype=np.float64) # numpy after 1.25 reads them as array with ndim=1
+            # t *= mi.tac_r / float(mi.tac_g * mi.adc_re)
+            t = np.arange(mi["adc_re"][0], dtype=np.float64)
+            t *= mi["tac_r"] / mi["tac_g"] * mi["adc_re"]
+
+            if "image_rx" in mi.dtype.names:
+                routing_channels_x = mi["image_rx"][0]
+            else:
+                routing_channels_x = 1
+
             times.append(t)
             offset = bh.next_block_offs
         return (times, [mi.scan_x[0], mi.scan_y[0], mi.adc_re[0], routing_channels_x])
-    
-    
+
+
 def read_sdt150(filename):
-    """ sdt bruker uses data_block001 instead of data_block"""
+    """sdt bruker uses data_block001 instead of data_block"""
     import warnings
+
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     t, XYTC = read_sdt_info_brukerSDT(filename)
-    
+
     with zipfile.ZipFile(filename) as myzip:
         z1 = myzip.infolist()[0]  # "data_block"
         with myzip.open(z1.filename) as myfile:
             dataspl = myfile.read()
-            
+
     dataSDT = np.fromstring(dataspl, np.uint16)
 
     if XYTC[3] > 1:
-        dataSDT = dataSDT[:XYTC[0] * XYTC[1] * XYTC[2] * XYTC[3]].reshape([XYTC[3], XYTC[0], XYTC[1], XYTC[2]])
-        if dataSDT[0, :, :, :].sum() == 0:  # bruker uses two channels and keeps one empty!!!
+        dataSDT = dataSDT[: XYTC[0] * XYTC[1] * XYTC[2] * XYTC[3]].reshape(
+            [XYTC[3], XYTC[0], XYTC[1], XYTC[2]]
+        )
+        if (
+            dataSDT[0, :, :, :].sum() == 0
+        ):  # bruker uses two channels and keeps one empty!!!
             dataSDT = np.squeeze(dataSDT[1, :, :, :])
         else:
-            if dataSDT[1, :, :, :].sum() == 0:  # bruker uses two channels and keeps one empty!!!
+            if (
+                dataSDT[1, :, :, :].sum() == 0
+            ):  # bruker uses two channels and keeps one empty!!!
                 dataSDT = np.squeeze(dataSDT[0, :, :, :])
                 # print ' ch1 is empty'
             else:
                 pass
     else:
-        dataSDT = dataSDT[:XYTC[0] * XYTC[1] * XYTC[2] * XYTC[3]].reshape([XYTC[0], XYTC[1], XYTC[2]])
-    #print("READ DATA IN:",dataSDT.shape)
-    return (dataSDT)
+        dataSDT = dataSDT[: XYTC[0] * XYTC[1] * XYTC[2] * XYTC[3]].reshape(
+            [XYTC[0], XYTC[1], XYTC[2]]
+        )
+    # print("READ DATA IN:",dataSDT.shape)
+    return dataSDT
 
 
 def read_bruker_sdt(filename):
@@ -105,4 +132,3 @@ def read_bruker_sdt(filename):
         data_blocks.append(block.copy())
 
     return data_blocks
-
